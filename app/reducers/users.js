870416091b2fbb99641f382159e625bc346f6b4c
Akshay Kanthi <aksanoble@gmail.com>
2017-08-14 16:48:49 +0530

    add users reducer

---
+++ app/reducers/users.js
@@ -0,0 +1,39 @@
+const AUTH_USER = "AUTH_USER";
+const UNAUTH_USER = "UNAUTH_USER";
+const FETCHING_USER = "FETCHING_USER";
+const FETCHING_USER_FAILURE = "FETCHING_USER_FAILURE";
+const FETCHING_USER_SUCCESS = "FETCHING_USER_SUCCESS";
+const REMOVE_FETCHING_USER = "REMOVE_FETCHING_USER";
+
+export const authUser = id => {
+  return {
+    type: AUTH_USER,
+    id: id
+  };
+};
+
+const initialState = {
+  1: {
+    name: "Mahesh Sharma",
+    description: "Director, Let's Venture"
+  },
+  isFetching: false,
+  type: "host",
+  authedId: 1,
+  isAuthed: "true",
+  error: ""
+};
+
+export default function users(state = initialState, action) {
+  switch (action.type) {
+    case AUTH_USER:
+      return {
+        ...state,
+        isAuthed: true,
+        authedId: action.id
+      };
+
+    default:
+      return state;
+  }
+}
