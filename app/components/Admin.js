d55815b9b3a32d8e07b3972ca5266e4c86c9b615
rabindrachhanchan <rabindra.chhanchan@mountblue.io>
2017-08-16 16:40:23 +0530

    initial commit on admin_redux

--- app/components/Admin.js
+++ app/components/Admin.js
@@ -1,4 +1,6 @@
+import { connect } from "react-redux";
 import React from "react";
+
 import Header from "./Header";
 import HeaderAma from "./HeaderAma";
 import AmaDetail from "./AmaDetail";
@@ -50,70 +52,8 @@ export default class Host extends React.Component {
                 numRegistration="100"/>
               <hr/>
           </div>
-          <div id="myModal" className="modal fade">
-              <div className="modal-dialog">
-                  <div className="modal-content">
-                      <div className="modal-header">
-                          <h2>Share AMA</h2>
-                      </div>
-                      <div className="modal-body">
-                          <div className="modalrow">
-                              <h4>Audience URL</h4>
-                          </div>
-                          <div className="modalrow">
-                              <div className="col-sm-10">
-                                  <input
-                                      className="form-control"
-                                      id="audienceUrlInput"
-                                      type="text"
-                                      value="www.letsventure.com/ama12/audience/asd12312"
-                                  />
-                              </div>
-                              <div className="col-sm-2">
-                                  <button
-                                      className="btn"
-                                      id="audienceUrlInputCopy"
-                                      type="button"
-                                      value="www.letsventure.com/ama12/audience/asd12312">
-                                      copy
-                                  </button>
-                              </div>
-                          </div>
-                          <div className="modalrow">
-                              <h4>Host URL</h4>
-                          </div>
-                          <div className="row">
-                              <div className="col-sm-10">
-                                  <input
-                                      className="form-control"
-                                      id="hostUrlInput"
-                                      type="text"
-                                      value="www.letsventure.com/ama12/audience/asd12312"
-                                  />
-                              </div>
-                              <div className="col-sm-2">
-                                  <button
-                                      className="btn"
-                                      id="hostUrlInputCopy"
-                                      type="button"
-                                      value="www.letsventure.com/ama12/host/asd12312">
-                                      copy
-                                  </button>
-                              </div>
-                          </div>
-                      </div>
-                      <div className="modal-footer">
-                          <button type="button" data-dismiss="modal" className="btn btn-primary text-right">
-                               Done
-                          </button>
-                      </div>
-                 </div>
-              </div>
-          </div>
-          <footer>
-              <address></address>
-         </footer>
       </div>
     )
   }
 }
+ connect()(Host);
