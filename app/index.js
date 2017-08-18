403dc1f25fcb86b1b5a84c3ac451a7b0b52965ed
rabindrachhanchan <rabindra.chhanchan@mountblue.io>
2017-08-18 18:01:30 +0530

    Add:Create ama component

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { store } from "./store";
-import { Host, Founder, Admin, Home } from "./components";
+import { Host, Founder, Admin,Home,Ama} from "./components";
 import { Provider, connect } from "react-redux";
 import {ref} from './config'
 
@@ -20,6 +20,7 @@ class App extends Component {
             <Route exact path="/host" component={Host} />
             <Route exact path="/founder" component={Founder} />
             <Route exact path="/admin" component={Admin} />
+            <Route exact path="/admin/ama" component={Ama} />
           </Switch>
         </Router>
       </Provider>
