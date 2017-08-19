8c7acc23f86959667501e2834e1bef643a7c61fd
rabindrachhanchan <rabindra.chhanchan@mountblue.io>
2017-08-19 16:49:09 +0530

    ADD: modify ama

--- app/components/Ama.js
+++ app/components/Ama.js
@@ -1,20 +1,69 @@
 import React from "react";
 import Header from "./Header";
+import { connect } from "react-redux";
 
-export default class Ama extends React.Component {
+class Ama extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        id:'',
+        sessionName:'',
+        date:'',
+        status:'',
+        title:'',
+        hostName:'',
+        hostImage:'',
+        hostLink:'',
+        description:"",
+    };
+
+  }
+  componentDidMount() {
+    var path=this.props.match.path;
+    var id=this.props.match.params.id;
+    const modifyAmaPath="/admin/ama/:id/modify"
+    if(!path==modifyAmaPath){
+      this.setState({
+        id:'',
+        sessionName:'',
+        date:'',
+        status:'',
+        time:'',
+        title:'Create New Ask Me Anything',
+      })
+    }else if(path==modifyAmaPath){
+       this.setState({
+          id:this.props.id,
+          sessionName:`AMA WITH ${this.props.ama[id].name}`,
+          date:this.props.ama[id].date,
+          banner:'choose',
+          status:this.props.ama[id].status,
+          time:this.props.ama[id].time,
+          hostName:'LetsVenture',
+          hostImage:'LetsVentureLogo',
+          hostLink:'LetsVenture link',
+          description:"Some details about the session",
+      })
+      }
+    }
   render() {
     return (
       <div className="create-ama-component">
         <Header />
         <div>
           <div >
-              <h2>Create New Ask Me Anything</h2>
+              <h2>{this.state.title}</h2>
           </div>
 
           <div >
               <div className="form-group">
                 <label>Name of the Session</label>
-                <input className="form-control" rows="2" id="comment"></input>
+                <input
+                  className="form-control"
+                  rows="2"
+                  value={this.state.sessionName}
+                  >
+                </input>
               </div>
           </div>
 
@@ -22,7 +71,10 @@ export default class Ama extends React.Component {
                <div className="form-group">
                   <label>Upload AMA Banner </label>
                   <div className="input-group col-xs-12">
-                    <input type="text" className="form-control"  placeholder="Select Image"/>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Select Image"/>
                     <span className="input-group-btn">
                       <button className="browse btn btn-primary" type="button">CHOOSE</button>
                     </span>
@@ -33,14 +85,22 @@ export default class Ama extends React.Component {
           <div >
               <div className="form-group">
                 <label>Schedule Time to Start</label>
-                <input className="form-control" rows="2"/>
+                <input
+                  className="form-control"
+                  rows="2"
+                  value={this.state.date}>
+                </input>
               </div>
           </div>
 
           <div >
               <div className="form-group">
                 <label>Duration(in Mins)</label>
-                <input className="form-control" rows="2"></input>
+                <input
+                    className="form-control"
+                    rows="2"
+                    value={this.state.time}>
+                </input>
               </div>
           </div>
           <hr/>
@@ -52,7 +112,10 @@ export default class Ama extends React.Component {
           <div>
               <div className="form-group">
                 <label>Name of Host </label>
-                <input className="form-control" rows="2"/>
+                <input
+                className="form-control"
+                rows="2"
+                value={this.state.hostName}/>
               </div>
           </div>
 
@@ -60,9 +123,13 @@ export default class Ama extends React.Component {
                <div className="form-group">
                   <label>Upload Host Image </label>
                   <div className="input-group col-xs-12">
-                    <input type="text" className="form-control "  placeholder="Select Image"/>
+                    <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Select Image"
+                    value={this.state.hostImage}/>
                     <span className="input-group-btn">
-                      <button className="browse btn btn-primary " type="button">CHOOSE</button>
+                      <button className="browse btn btn-primary" type="button">CHOOSE</button>
                     </span>
                   </div>
               </div>
@@ -71,7 +138,9 @@ export default class Ama extends React.Component {
           <div >
               <div className="form-group">
                 <label>Host Profile Link </label>
-                <input className="form-control" ></input>
+                <input
+                className="form-control"
+                value={this.state.hostLink}></input>
               </div>
           </div>
 
@@ -79,7 +148,7 @@ export default class Ama extends React.Component {
               <div className="form-group">
                 <label>Description of AMA
                      <textarea className="form-control" rows="15" cols="80">
-
+                        {this.state.description}
                      </textarea>
                 </label>
               </div>
@@ -95,3 +164,8 @@ export default class Ama extends React.Component {
     )
   }
 }
+
+const mapStateToProps = state => {
+  return state
+};
+export default connect(mapStateToProps)(Ama);
